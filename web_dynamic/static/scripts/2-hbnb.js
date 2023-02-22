91710003508fd69bbd6a54a651e1dcc0bef5d1d5
Popsicool <akinolasamson1234@gmail.com>
2023-02-22 08:56:50 +0100

    task 2-4

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -9,8 +9,11 @@ $("document").ready(function () {
         $(".amenities h4").text(Object.keys(amenities).sort().join(", "));
     });
     $.getJSON("http://127.0.0.1:5001/api/v1/status/", (response) =>{
+        console.log(response.status)
         if (response.status === "OK") {
             $("div#api_status").addClass("available");
+            $('div#api_status').removeAttr('id');
+            
         } else {
             $("div#api_status").removeClass("available");
         }
