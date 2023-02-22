742621f883da372f03b51a3e78517c02f29c762d
Popsicool <akinolasamson1234@gmail.com>
2023-02-22 11:45:00 +0100

    task 5

--- web_dynamic/static/scripts/4-hbnb.js
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -23,7 +23,7 @@ $("document").ready(function () {
         headers: {
             "Content-Type": "application/json",
         },
-        success:(response) => {
+        success: (response) => {
             response.forEach((place) => {
                 $("section.places").append(
                     `<article>
@@ -32,7 +32,7 @@ $("document").ready(function () {
         <div class="price_by_night">${place.price_by_night}</div>
     </div>
     <div class="information">
-        <div class="max_guest">${place.max_guest} Guest${ place.max_guest != 1 ? "s" : ""}  </div>
+        <div class="max_guest">${place.max_guest} Guest${place.max_guest != 1 ? "s" : ""}  </div>
         <div class="number_rooms">${place.number_rooms} Bedroom${place.number_rooms != 1 ? "s" : ""}</div>
         <div class="number_bathrooms">${place.number_bathrooms} Bathroom${place.number_bathrooms != 1 ? "s" : ""}</div>
     </div>
@@ -43,5 +43,36 @@ $("document").ready(function () {
                 )
             });
         }
+    });
+
+    $(".filters button").click(function(){
+        $.post({
+            url: "http://127.0.0.1:5001/api/v1/places_search/",
+            data: JSON.stringify(amenities),
+            headers: {
+                "Content-Type": "application/json",
+            },
+            success: (response) => {
+                response.forEach((place) => {
+                    $("section.places").append(
+                        `<article>
+    <div class="title_box">
+        <h2>${place.name}</h2>
+        <div class="price_by_night">${place.price_by_night}</div>
+    </div>
+    <div class="information">
+        <div class="max_guest">${place.max_guest} Guest${place.max_guest != 1 ? "s" : ""}  </div>
+        <div class="number_rooms">${place.number_rooms} Bedroom${place.number_rooms != 1 ? "s" : ""}</div>
+        <div class="number_bathrooms">${place.number_bathrooms} Bathroom${place.number_bathrooms != 1 ? "s" : ""}</div>
+    </div>
+    <div class="description">
+        ${place.description}
+    </div>
+</article>`
+                    )
+                });
+            }
+        });
     })
+
 });
